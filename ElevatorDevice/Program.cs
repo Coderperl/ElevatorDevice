8dc8e1e221ee6bba7a684dcc18758c4bba1b8c19
Joseph OBrien <Joseph.obrien97@hotmail.com>
2022-11-17 11:06:48 +0100

    Nollställ elevator directmethod

--- ElevatorDevice/Program.cs
+++ ElevatorDevice/Program.cs
@@ -17,7 +17,7 @@ namespace ElevatorDevice
     {
         private static DeviceClient _deviceClient;        
         private static Twin twin;
-        private static string elevatorapi = "https://agilewebapi.azurewebsites.net/api/Elevator";
+        private static string elevatorapi = "https://localhost:7169/api/Elevator";
         private static string apiUri = "https://localhost:7169/api";
         public static List<ElevatorItem> elevatorItems;
         private static int _Intervall = 5000;        
@@ -57,9 +57,11 @@ namespace ElevatorDevice
                 twinCollection["elevators"] = elevatorItems;
                 await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
                 await _deviceClient.SetMethodHandlerAsync("ShutDown", ShutDown, _deviceClient);
+                await _deviceClient.SetMethodHandlerAsync("Reset", Reset, _deviceClient);
             }                                  
 
             await _deviceClient.SetMethodHandlerAsync("ShutDown", ShutDown, _deviceClient);
+            await _deviceClient.SetMethodHandlerAsync("Reset", Reset, _deviceClient);
 
             _connected = true;            
 
@@ -106,7 +108,6 @@ namespace ElevatorDevice
                 var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
                 await client.PutAsync($"{apiUri}/Elevator/{elevator.Id}", httpContent);
             }
-            
         }
         private static async Task<MethodResponse> ShutDown(MethodRequest request, object userContext)
         {
@@ -121,7 +122,50 @@ namespace ElevatorDevice
 
             return await Task.FromResult(new MethodResponse(new byte[0], 200));
         }
-        
+
+        private static async Task<MethodResponse> Reset(MethodRequest request, object userContext)
+        {
+            try
+            {
+                await PrintReset(request.DataAsJson);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return await Task.FromResult(new MethodResponse(new byte[0], 200));
+        }
+        private static async Task PrintReset(string request = null)
+        {
+            if (request.Equals("null"))
+            {
+                Console.WriteLine("Request null");
+            }
+            else
+            {
+                var result = JsonConvert.DeserializeObject<dynamic>(request);
+                Console.WriteLine(result.id);
+                List<ElevatorItem> elevators = twin.Properties.Reported["elevators"].ToObject<List<ElevatorItem>>();
+
+                var elevator = elevators.Find(e => e.Id == Convert.ToInt16(result.id));
+                Console.WriteLine("Reset was : " + elevator.Reboot);
+                elevator.Reboot = !elevator.Reboot;
+                elevator.Floor = elevator.MinFloor;
+                var twinCollection = new TwinCollection();
+                twinCollection["elevators"] = elevators;
+                await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+
+                Console.WriteLine("Reset became : " + elevator.Reboot);
+
+                using var client = new HttpClient();
+
+                var payload = JsonConvert.SerializeObject(elevator);
+                var httpContent = new StringContent(payload, Encoding.UTF8, "application/json");
+                await client.PutAsync($"{apiUri}/Elevator/{elevator.Id}", httpContent);
+            }
+
+        }
     }
     
 }
